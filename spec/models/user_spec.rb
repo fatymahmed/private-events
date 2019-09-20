9350f29f6a2551859ff45cf06ae3b1698cb94d25
fatima ahmed <fatymah001@gmail.com>
2019-09-21 00:02:35 +0300

    Implement unit tests for invalid user

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,13 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  user=User.create(name: "fatima", email: "fatima@g.gmail")
+  before :each do
+    @user=User.new(name: "fatima", email: "fatima@g.gmail")
+  end 
   context "valid User" do
     it "has a valid email" do
-      expect(user).to be_valid
+      expect(@user).to be_valid
     end
     it "has a valid name" do
-      expect(user).to be_valid
+      expect(@user).to be_valid
+    end
+  end
+
+  context "invalid User" do
+    it "has a invalid email" do
+      @user.email=" "
+      expect(@user).to_not be_valid
+    end
+    it "has a valid name" do
+      @user.name="  "
+      expect(@user).to_not be_valid
     end
   end
 end
