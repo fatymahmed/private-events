4c0a75242232218d16d658e2dc3ba5020d953320
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-21 04:01:16 +0530

    Imliment User controller with tests

--- spec/features/user_signup_spec.rb
+++ spec/features/user_signup_spec.rb
@@ -6,8 +6,8 @@ RSpec.describe 'user signup', type: :feature do
     visit new_user_path
     fill_in('Name', with: 'Example')
     fill_in('Email', with: 'example@email.com')
-    click_link('Submit')
-    expect(page).to have_css('user')
+    click_button('Submit')
+    expect(page).to have_css('.user')
     expect(page).to have_content('Example')
   end
   
