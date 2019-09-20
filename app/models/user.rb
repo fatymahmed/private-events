fc9f28619a1b81fd32e74a434b5eefc88c86a089
fatima ahmed <fatymah001@gmail.com>
2019-09-20 23:24:34 +0300

    Implement User model and its validations

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+	before_save :downcase_email
+	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
+
+  validates :name, presence: true, length: { maximum: 150 }
+  validates :email, presence: true, length: { maximum: 150 },
+                    uniqueness: { case_sensitive: false }, format: { with: VALID_EMAIL_REGEX }
+
+  validates :password, presence: true, length: { in: 6..20 }
+
+
+	private
+	def downcase_email
+		email.downcase!
+	end
+end
+
