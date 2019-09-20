43333d7e2a1a24f43e6a5fefbaca35ff828e4f8d
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-21 04:19:12 +0530

    TODO: Finish tests for User signup on Monday

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,12 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  
+  def sign_in_as(user)
+    session[:email] = user.email
+  end
+  
+  def current_user
+    @current_user ||= User.find_by(email: session[:email])
+  end
+  
 end
