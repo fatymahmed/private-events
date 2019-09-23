f6e1eb693ffd719c81951353b040c97ac3249a37
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-23 22:44:46 +0530

    ss

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,7 +1,8 @@
 class Event < ApplicationRecord
-	belongs_to :user, :class_name => "User", :foreign_key => 'creator_id'
+	belongs_to :creator, class_name: "User", foreign_key: 'user_id'
 	validates :description, presence: true, length: {maximum: 200}
 	validates_presence_of :date
 	validates :location, presence: true, length: {maximum: 100}
 end
+
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190923160704) do
+ActiveRecord::Schema.define(version: 20190923170526) do
 
   create_table "events", force: :cascade do |t|
     t.text "description"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(version: 20190923160704) do
     t.string "location"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", force: :cascade do |t|
