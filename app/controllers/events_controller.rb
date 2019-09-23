d9f516d85a7a3b783f39d357998da558468c08b0
fatima ahmed <fatymah001@gmail.com>
2019-09-23 23:13:18 +0300

    Implement create event using TDD

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -1,13 +1,34 @@
 class EventsController < ApplicationController
+  before_action :logged_in
   def new
+    @event=Event.new()
   end
 
   def create
+    @event=current_user.events.build(events_params)
+    if @event.save
+      redirect_to @event
+    else
+      render 'new'
+    end
   end
 
   def index
   end
 
   def show
+    @event=Event.find(params[:id])
+  end
+
+  private
+
+  def events_params
+    params.require(:event).permit(:description, :date, :location)
+  end
+
+  def logged_in
+    if current_user.nil?
+      redirect_to new_user_path
+    end
   end
 end
---
+++ spec/features/user_create_event_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe 'user creates event', type: :feature do
+	scenario "successfully with valid information" do
+		visit new_event_path
+		expect(page).to have_current_path(new_user_path)
+		fill_in('Name', with: 'Example')
+    fill_in('Email', with: 'example@email.com')
+		click_button('Submit')
+		visit new_event_path
+		fill_in('Description', with: 'an event')
+		fill_in('Date', with: Date.today)
+		fill_in('Location', with: "microverse")
+		click_button('Submit')
+		expect(page).to have_css('.event')
+	end
+end
