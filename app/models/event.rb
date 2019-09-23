11fef4ab3a0ad592cc23ac6b73c9a1e5f60c0873
fatima ahmed <fatymah001@gmail.com>
2019-09-23 19:39:25 +0300

    Implement validations for event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,7 @@
 class Event < ApplicationRecord
-    belongs_to :user , foreign_key: creator_id, class: User
+	belongs_to :user, :class_name => "User", :foreign_key => 'creator_id'
+	validates :description, presence: true, length: {maximum: 200}
+	validates_presence_of :date
+	validates :location, presence: true, length: {maximum: 100}
 end
+
--- spec/models/event_spec.rb
+++ spec/models/event_spec.rb
@@ -1,5 +1,36 @@
 require 'rails_helper'
 
 RSpec.describe Event, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user=User.new(name: "example", email: "example@user.com")
+    @event=@user.events.new(description: "Microverse assembly", date: Date.now, location: "zoom")
+  end
 end
+
+
+
+# RSpec.describe User, type: :model do
+#   before :each do
+#     @user=User.new(name: "fatima", email: "fatima@g.gmail")
+#   end 
+#   context "valid User" do
+#     it "has a valid email" do
+#       expect(@user).to be_valid
+#     end
+#     it "has a valid name" do
+#       expect(@user).to be_valid
+#     end
+#   end
+
+#   context "invalid User" do
+#     it "has a invalid email" do
+#       @user.email=" "
+#       expect(@user).to_not be_valid
+#     end
+#     it "has a valid name" do
+#       @user.name="  "
+#       expect(@user).to_not be_valid
+#     end
+#   end
+
+# end
