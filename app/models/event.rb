dd28264573d1d7060bb88492a42478fd181ef0a8
fatima ahmed <fatymah001@gmail.com>
2019-09-23 19:17:54 +0300

    Add associations between user and event model

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,3 @@
 class Event < ApplicationRecord
+    belongs_to :user , foreign_key: creator_id, class: User
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ApplicationRecord
+	has_many :events
 	before_save :downcase_email
 	VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i.freeze
 
