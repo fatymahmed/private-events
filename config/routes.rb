91dc241c02991ad3ab524ad6b4bcaa0fb2228d8d
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-25 01:18:03 +0530

    Add styles to navigation and events

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :user, only: [:new, :create, :show]
 
   resources :events, only: [:new, :create, :index, :show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root to: 'events#index'
 end
