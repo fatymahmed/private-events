acffc25e36794fe09364ecf816c634e413d8f619
fatima ahmed <fatymah001@gmail.com>
2019-09-25 00:01:22 +0300

    Implement unit test for valid attendance

--- spec/models/attendance_spec.rb
+++ spec/models/attendance_spec.rb
@@ -3,5 +3,18 @@
 require 'rails_helper'
 
 RSpec.describe Attendance, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before :each do
+    @user = User.create(name: 'example', email: 'example@user.com')
+    @event = @user.events.create(description: 'Microverse assembly', date: Date.today, location: 'zoom')
+    @attendance=Attendance.new(attendee: @user,attended_event: @event)
+  end
+  context 'valid Attendance' do
+    it 'with a valid user' do
+      expect(@attendance).to be_valid
+    end
+    it 'with a valid event' do
+      expect(@attendance).to be_valid
+    end
+  end
 end
+  
