cfbd3243264a53675eb24369c8348a17fc6e7053
fatima ahmed <fatymah001@gmail.com>
2019-09-24 23:43:40 +0300

    Fix user resource

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  resources :user, only: [:new, :create, :show]
+  resources :users, only: [:new, :create, :show]
 
   resources :events, only: [:new, :create, :index, :show]
   root to: 'events#index'
