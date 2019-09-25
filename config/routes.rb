c46c87efc3d2f2b1406edec1bda60944852d7e36
Shivam Kaushik <shivam.kaushik@outlook.com>
2019-09-26 01:29:43 +0530

    TODO: Fix event attendance association

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,6 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show, :destroy]
 
   resources :events, only: [:new, :create, :index, :show]
+  
   root to: 'events#index'
 end
